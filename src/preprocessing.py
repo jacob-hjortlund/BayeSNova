819f328bf53a707d859a50d8bd469bba13e1ec53
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2022-11-30 17:05:13 +0100

    updated shape of covariances so first dim is observations

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,7 +5,7 @@ import src.utils as utils
 
 def build_covariance_matrix(data: np.ndarray, offset: float = 1e-323) -> np.ndarray:
     """Given a data array with shape (N,M), populate a set of 
-    covariance matrices with the shape (3,3,N). If any covariance
+    covariance matrices with the shape (N,3,3). If any covariance
     matrix has a zero or negative determinant, a small value is
     added along the diagonal.
 
@@ -13,29 +13,27 @@ def build_covariance_matrix(data: np.ndarray, offset: float = 1e-323) -> np.ndar
         data (np.ndarray): input array with shape (N,M)
 
     Returns:
-        np.ndarray: (3,3,N) covariance matrix
+        np.ndarray: (N,3,3) covariance matrix
     """
 
-    cov = np.zeros((3, 3, data.shape[0]))
+    cov = np.zeros((data.shape[0], 3, 3))
 
     # diagonals
-    cov[0, 0, :] = data[:, 1] ** 2 # omega_m^2
-    cov[1, 1, :] = data[:, 3] ** 2 # omega_x^2
-    cov[2, 2, :] = data[:, 5] ** 2 # omega_colour^2
+    cov[:,0,0] = data[:, 1] ** 2 # omega_m^2
+    cov[:,1,1] = data[:, 3] ** 2 # omega_x^2
+    cov[:,2,2] = data[:, 5] ** 2 # omega_colour^2
 
     # upper off-diagonals
-    cov[0, 1, :] = data[:, 8] * (-2.5 / (np.log(10.) * data[:, 6]))
-    cov[0, 2, :] = data[:, 9] * (-2.5 / (np.log(10.) * data[:, 6]))
-    cov[1, 2, :] = data[:, 7]
+    cov[:,0,1] = data[:, 8] * (-2.5 / (np.log(10.) * data[:, 6]))
+    cov[:,0,2] = data[:, 9] * (-2.5 / (np.log(10.) * data[:, 6]))
+    cov[:,1,2] = data[:, 7]
 
     # lower off-diagonals
-    cov[1, 0, :] = cov[0, 1, :]
-    cov[2, 0, :] = cov[0, 2, :]
-    cov[2, 1, :] = cov[1, 2, :]
+    cov[:,1,0] = cov[:,0,1]
+    cov[:,2,0] = cov[:,0,2]
+    cov[:,2,1] = cov[:,1,2]
 
     # add constant to covs with negative determinant
-    cov_swapped = cov.swapaxes(0,2) # axis swapped view of cov
-    posdef_covs = utils.ensure_posdef(cov_swapped)
-    posdef_covs = posdef_covs.swapaxes(0,2)
-
+    posdef_covs = utils.ensure_posdef(cov)
+    
     return posdef_covs
