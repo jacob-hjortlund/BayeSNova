b06f70c64c00ced92a90c00b39271e64143d75a7
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2022-11-30 17:05:57 +0100

    updated to use float64 eps

--- src/utils.py
+++ src/utils.py
@@ -36,9 +36,7 @@ def theta_to_dict(
 
     return arg_dict
 
-def ensure_posdef(
-    covs: np.ndarray, offset: float = 1e-323
-) -> np.ndarray:
+def ensure_posdef(covs: np.ndarray) -> np.ndarray:
     """Given an array of covariance matrices, check if any are not
     positive definite and, if any are found, add a small offset 
     on the diagonal to ensure positive definiteness.
@@ -46,15 +44,13 @@ def ensure_posdef(
     Args:
         covs (np.ndarray): Array of covaraince matrices with shape
         (N_COVS, DIM, DIM)
-        offset (float, optional): Diagonal offset. Defaults to 1e-323.
-
     Returns:
         np.ndarray: _description_
     """
 
     idx_neg_det = np.linalg.det(covs) <= 0
     covs[idx_neg_det] += np.diag(
-        np.ones(covs.shape[1]) * offset
+        np.ones(covs.shape[1]) * np.finfo("float64").eps
     )
 
     return covs
