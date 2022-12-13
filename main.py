e4a7b180d28249e00edac9c810ba29a6e695e478
jacob-hjortlund <jacob.hjortlund@gmail.com>
2022-12-13 12:06:11 +0100

    minor

--- main.py
+++ main.py
@@ -40,57 +40,32 @@ def main(cfg: omegaconf.DictConfig) -> None:
         n_workers=cfg['emcee_cfg']['n_workers']
     )
 
-    init_pos = np.array([
-        -19.3,-0.14,3.1,-0.25, 1.1, -0.09, 0.05,3.7,0.04,0.6,2.9, -19.3,0.1, 1.1, -0.09,0.04,0.4, 0.03 
-    ])
-    theta = np.array([-0.14, 3.1, 3.7, 0.6, 2.9, -19.3, -19.3, -0.09, -0.09, 0.05, 0.03, -0.25, 0.1, 1.1, 1.1, 0.04, 0.04, 0.4])
-    n_pars = len(theta)
-    idx = np.empty(n_pars)
-
     # call func once befor loop to jit compile
-    _ = log_prob(theta)
-    # t0 = time()
-    # with swbd.MPIPool() as pool:
-    #     if not pool.is_master():
-    #         pool.wait()
-    #         sys.exit(0)
-    #     theta = np.array([
-    #         -0.14, 3.1, 3.7, 0.6, 2.9, -19.3, -19.3, -0.09, -0.09, 0.05, 0.03, -0.25, 0.1, 1.1, 1.1, 0.04, 0.04, 0.4
-    #     ]) +3e-2 * np.random.rand(cfg['emcee_cfg']['n_walkers'],18)
-    #     nwalkers, ndim = theta.shape
-    #     sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool)
-    #     sampler.run_mcmc(theta, cfg['emcee_cfg']['n_steps'])
-    # avg_eval_time = (time()-t0) / (cfg['emcee_cfg']['n_walkers'] * cfg['emcee_cfg']['n_steps'])
-    # print(f"Avg. time pr. step: {avg_eval_time} s")
-        
-    # samples = sampler.get_chain()
-    # np.savez_compressed('/groups/dark/osman/Thesis/data/full_chain2',results=samples)
-    # flat_samples = sampler.get_chain(discard=500, thin=48, flat=True)
-    # print(flat_samples.shape)
-    # np.savez_compressed('/groups/dark/osman/Thesis/data/reduced_chain2',results=flat_samples)
-    # tau = sampler.get_autocorr_time()
-    # print(tau)
-
-    for i in range(n_pars):
-        tmp_idx = theta == init_pos[i]
-        idx[tmp_idx] = i
-
-    chains = np.load('./data/test.npz')['results'][100:499, idx.astype('int')]#[:10]
-
-    log_p = 0.
     t0 = time()
-    for chain_theta in tqdm(chains):
-        log_p += log_prob(chain_theta)
-    t_tot = time()-t0
-    print("LogP:", log_p)
-    print("Total time logged:", t_tot)
-    print("Avg time pr. evaluation:", t_tot / len(chains))
-    
-    # # Setup MCMC
-    # dims = len(cfg['model_cfg']['shared_par_names']) + 2 * len(cfg['model_cfg']['independent_par_names']) + 1
-    # init = np.random.rand(100,dims)
-    # sampler = em.EnsembleSampler(100, dims, log_prob)
-    # sampler.run_mcmc(init, 1000, skip_initial_state_check=True, progress=True)
+    with swbd.MPIPool() as pool:
+        if not pool.is_master():
+            pool.wait()
+            sys.exit(0)
+        theta = np.array([
+            -0.14, 3.1, 3.7, 0.6, 2.9, -19.3, -19.3, -0.09, -0.09, 0.05, 0.03, -0.25, 0.1, 1.1, 1.1, 0.04, 0.04, 0.4
+        ]) +3e-2 * np.random.rand(cfg['emcee_cfg']['n_walkers'],18)
+        _ = log_prob(theta[0])
+        nwalkers, ndim = theta.shape
+        sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool)
+        sampler.run_mcmc(theta, cfg['emcee_cfg']['n_steps'])
+    t1 = time()
+    total_time = t1-t0
+    avg_eval_time = (total_time) / (cfg['emcee_cfg']['n_walkers'] * cfg['emcee_cfg']['n_steps'])
+    print(f"Total time:", total_time)
+    print(f"Avg. time pr. step: {avg_eval_time} s")
+        
+    samples = sampler.get_chain()
+    np.savez_compressed('/groups/dark/osman/Thesis/data/baseline_full_chain',results=samples)
+    flat_samples = sampler.get_chain(discard=500, thin=48, flat=True)
+    print(flat_samples.shape)
+    np.savez_compressed('/groups/dark/osman/Thesis/data/baseline_reduced_chain',results=flat_samples)
+    tau = sampler.get_autocorr_time()
+    print(tau)
 
 if __name__ == "__main__":
     main()
