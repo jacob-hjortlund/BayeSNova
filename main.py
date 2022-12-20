7dc92d5c5d39082df00b0011eaf309e95552b914
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2022-12-20 09:15:51 +0100

    Refactored to ensure init only once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import hydra
 import omegaconf
 import sys
+import os
 
 import emcee as em
 import numpy as np
@@ -17,10 +18,11 @@ from tqdm import tqdm
 @hydra.main(
     version_base=None, config_path="configs", config_name="config"
 )
-def main(cfg: omegaconf.DictConfig) -> None:
+def main(cfg: omegaconf.DictConfig) -> None:   
 
-    # Print cfg
-    print(cfg.pretty())    
+    # Setup results dir
+    path = cfg['emcee_cfg']['save_path']
+    os.makedirs(path, exist_ok=True)
 
     # Import data
     data = pd.read_csv(
@@ -43,32 +45,50 @@ def main(cfg: omegaconf.DictConfig) -> None:
         n_workers=cfg['emcee_cfg']['n_workers']
     )
 
-    # call func once befor loop to jit compile
     t0 = time()
     with swbd.MPIPool() as pool:
         if not pool.is_master():
             pool.wait()
             sys.exit(0)
+
+        # Print cfg
+        print("\n----------------- CONFIG ---------------------\n")
+        print(omegaconf.OmegaConf.to_yaml(cfg),"\n")
+
+        # Setup init pos and log_prob
         theta = np.array([
-            -0.14, 3.1, 3.7, 0.6, 2.9, -19.3, -19.3, -0.09, -0.09, 0.05, 0.03, -0.25, 0.1, 1.1, 1.1, 0.04, 0.04, 0.4
-        ]) +3e-2 * np.random.rand(cfg['emcee_cfg']['n_walkers'],18)
-        _ = log_prob(theta[0])
+                -0.14, 3.1, 3.7, 0.6, 2.9, -19.3, -19.3, -0.09, -0.09, 0.05, 0.03, -0.25, 0.1, 1.1, 1.1, 0.04, 0.04, 0.4
+            ]) +3e-2 * np.random.rand(cfg['emcee_cfg']['n_walkers'],18)
+        _ = log_prob(theta[0]) # call func once befor loop to jit compile
         nwalkers, ndim = theta.shape
-        sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool)
+
+        # Setup emcee backend
+        filename = os.path.join(
+            path, cfg['emcee_cfg']['file_name']
+        )
+        backend = em.backends.HDFBackend(filename, name=cfg['emcee_cfg']['run_name'])
+        if not cfg['emcee_cfg']['continue_from_sample']:
+            backend.reset(nwalkers, ndim)
+
+        sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, pool=pool, backend=backend)
         sampler.run_mcmc(theta, cfg['emcee_cfg']['n_steps'])
+    
     t1 = time()
     total_time = t1-t0
     avg_eval_time = (total_time) / (cfg['emcee_cfg']['n_walkers'] * cfg['emcee_cfg']['n_steps'])
-    print(f"Total time:", total_time)
-    print(f"Avg. time pr. step: {avg_eval_time} s")
+    print(f"\nTotal MCMC time:", total_time)
+    print(f"Avg. time pr. step: {avg_eval_time} s\n")
         
-    samples = sampler.get_chain()
-    np.savez_compressed('/groups/dark/osman/Thesis/data/baseline_full_chain',results=samples)
-    flat_samples = sampler.get_chain(discard=500, thin=48, flat=True)
-    print(flat_samples.shape)
-    np.savez_compressed('/groups/dark/osman/Thesis/data/baseline_reduced_chain',results=flat_samples)
     tau = sampler.get_autocorr_time()
-    print(tau)
+    print("Atuocorr length:\n", tau, "\n")
+    
+    # samples = sampler.get_chain()
+    # flat_samples = sampler.get_chain(discard=500, thin=48, flat=True)
+    # np.savez_compressed(
+    #     '/groups/dark/osman/Thesis/data/baseline_full_chain',results=samples
+    # )
+    # print(flat_samples.shape)
+    # np.savez_compressed('/groups/dark/osman/Thesis/data/baseline_reduced_chain',results=flat_samples)
 
 if __name__ == "__main__":
     main()
