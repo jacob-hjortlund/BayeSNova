462e6e88f2345648025e195bb0aa3de7ccf4c488
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-01-19 13:27:11 +0100

    temporary commit replacing numerical gamma dist for Rb with gauss

--- src/probabilities.py
+++ src/probabilities.py
@@ -3,6 +3,7 @@ import numba as nb
 import scipy as sp
 import src.utils as utils
 import multiprocessing as mp
+import scipy.stats as stats
 import scipy.special as sp_special
 import scipy.integrate as sp_integrate
 from functools import partial
@@ -121,20 +122,20 @@ def _population_r(
 def dbl_integral_body(
     x, y, i1, i2, i3, i5,
     i6, i9, r1, r2, r3,
-    Rb, gamma_Rb, shift_Rb,
+    Rb, gamma_Rb, #shift_Rb,
     Ebv, gamma_Ebv
 ):  
 
-    tau_Rb = Rb / gamma_Rb
+    #tau_Rb = Rb / gamma_Rb
     tau_Ebv = Ebv / gamma_Ebv
-    transformed_shift_Rb = shift_Rb / tau_Rb
-    x_shift = x - transformed_shift_Rb
+    #transformed_shift_Rb = shift_Rb / tau_Rb
+    x_shift = x #- transformed_shift_Rb
 
-    if x_shift < 0.:
-        return 0.
+    # if x_shift < 0.:
+    #     return 0.
 
     # update res and cov
-    r1 -= x_shift * tau_Rb * y * tau_Ebv
+    r1 -= x_shift * y * tau_Ebv #x_shift * tau_Rb * y * tau_Ebv
     r3 -= y * tau_Ebv
 
     # precalcs
@@ -148,16 +149,20 @@ def dbl_integral_body(
 
     # # calculate prob
     r_inv_cov_r = det_m1 * (r1 * r1 * A1 + r2 * r2 * A5 + r3 * r3 * A9 + 2 * (r1 * r2 * A2 + r1 * r3 * A3 + r2 * r3 * A6))
-    exponent_Rb = gamma_Rb - 1.
+    #exponent_Rb = gamma_Rb - 1.
     exponent_Ebv = gamma_Ebv - 1.
-    value = np.exp(-0.5 * r_inv_cov_r - x_shift - y) * x_shift**exponent_Rb * y**exponent_Ebv * det_m1**0.5
+    #value = np.exp(-0.5 * r_inv_cov_r - x_shift - y) * x_shift**exponent_Rb * y**exponent_Ebv * det_m1**0.5
+    value = (
+        np.exp(-0.5 * r_inv_cov_r - x_shift - y) * y**exponent_Ebv * det_m1**0.5 *
+        1. / (gamma_Rb * (2*np.pi)**0.5) * np.exp(-0.5 * ((x_shift - Rb) / gamma_Rb)**2)
+    )
 
     return value
 
 @nb.cfunc(quadpack_sig)
 def Rb_integral(x, data):
 
-    _data = nb.carray(data, (20,))
+    _data = nb.carray(data, (22,))
     i1 = _data[0]
     i2 = _data[1]
     i3 = _data[2]
@@ -183,10 +188,13 @@ Rb_integral_ptr = Rb_integral.address
 
 @nb.cfunc(quadpack_sig)
 def Ebv_integral(y, data):
-    _data = nb.carray(data, (19,))
+    _data = nb.carray(data, (21,))
     _new_data = np.concatenate(
         (_data, np.array([y]))
     )
+
+    if y < _data[-5] or y > _data[-4]:
+        return 0.
 
     inner_value, _, _ = dqags(
         Rb_integral_ptr, _data[-2], _data[-1], _new_data
@@ -200,9 +208,10 @@ def _fast_dbl_prior_convolution(
     cov_1: np.ndarray, res_1: np.ndarray,
     cov_2: np.ndarray, res_2: np.ndarray,
     Rb_1: float, gamma_Rb_1: float, Ebv_1: float, gamma_Ebv_1: float,
+    lower_bound_Ebv_1: float, upper_bound_Ebv_1: float,
     Rb_2: float, gamma_Rb_2: float, Ebv_2: float, gamma_Ebv_2: float,
-    shift_Rb: float = 0., lower_bound_Rb: float = 0., upper_bound_Rb: float = 10.,
-    lower_bound_Ebv: float = 0., upper_bound_Ebv: float = 10.
+    lower_bound_Ebv_2: float, upper_bound_Ebv_2: float,
+    shift_Rb: float, lower_bound_Rb: float, upper_bound_Rb: float
 ):
 
     n_sn = len(cov_1)
@@ -211,11 +220,13 @@ def _fast_dbl_prior_convolution(
     params_1 = np.array([
         Rb_1, gamma_Rb_1,
         Ebv_1, gamma_Ebv_1,
+        lower_bound_Ebv_1, upper_bound_Ebv_1,
         shift_Rb, lower_bound_Rb, upper_bound_Rb
     ])
     params_2 = np.array([
         Rb_2, gamma_Rb_2,
         Ebv_2, gamma_Ebv_2,
+        lower_bound_Ebv_2, upper_bound_Ebv_2,
         shift_Rb, lower_bound_Rb, upper_bound_Rb
     ])
 
@@ -229,10 +240,10 @@ def _fast_dbl_prior_convolution(
         )).copy()
         tmp_params_2.astype(np.float64)
         prob_1, _, s1 = dqags(
-            integrate_ptr, lower_bound_Ebv, upper_bound_Ebv, tmp_params_1
+            integrate_ptr, lower_bound_Ebv_1, upper_bound_Ebv_1, tmp_params_1
         )
         prob_2, _, s2 = dqags(
-            integrate_ptr, lower_bound_Ebv, upper_bound_Ebv, tmp_params_2
+            integrate_ptr, lower_bound_Ebv_2, upper_bound_Ebv_2, tmp_params_2
         )
 
         probs[i, 0] = prob_1
@@ -250,23 +261,26 @@ def _wrapper_dbl_prior_conv(
     shift_Rb: float = 0., lower_bound_Rb: float = 0., upper_bound_Rb: float = 10.,
     lower_bound_Ebv: float = 0., upper_bound_Ebv: float = 10.
 ):
+    lower_bound_Ebv_1, upper_bound_Ebv_1 = np.array([lower_bound_Ebv, upper_bound_Ebv]) * Ebv_1 / gamma_Ebv_1
+    lower_bound_Ebv_2, upper_bound_Ebv_2 = np.array([lower_bound_Ebv, upper_bound_Ebv]) * Ebv_2 / gamma_Ebv_2
     norm_1 = (
-        sp_special.gammainc(gamma_Rb_1, upper_bound_Rb) * sp_special.gamma(gamma_Rb_1) *
-        sp_special.gammainc(gamma_Ebv_1, upper_bound_Ebv) * sp_special.gamma(gamma_Ebv_1)
+        (stats.norm.cdf(upper_bound_Rb, loc=Rb_1, scale=gamma_Rb_1) - stats.norm.cdf(upper_bound_Rb, loc=Rb_1, scale=gamma_Rb_1)) *
+        #sp_special.gammainc(gamma_Rb_1, upper_bound_Rb) * sp_special.gamma(gamma_Rb_1) *
+        sp_special.gammainc(gamma_Ebv_1, upper_bound_Ebv_1) * sp_special.gamma(gamma_Ebv_1)
     )
     norm_2 = (
-        sp_special.gammainc(gamma_Rb_2, upper_bound_Rb) * sp_special.gamma(gamma_Rb_2) *
-        sp_special.gammainc(gamma_Ebv_2, upper_bound_Ebv) * sp_special.gamma(gamma_Ebv_2)
+        (stats.norm.cdf(upper_bound_Rb, loc=Rb_2, scale=gamma_Rb_2) - stats.norm.cdf(upper_bound_Rb, loc=Rb_2, scale=gamma_Rb_2)) *
+        #sp_special.gammainc(gamma_Rb_2, upper_bound_Rb) * sp_special.gamma(gamma_Rb_2) *
+        sp_special.gammainc(gamma_Ebv_2, upper_bound_Ebv_2) * sp_special.gamma(gamma_Ebv_2)
     )
 
     probs, status = _fast_dbl_prior_convolution(
         covs_1, r_1, covs_2, r_2,
-        Rb_1=Rb_1, gamma_Rb_1=gamma_Rb_1,
-        Ebv_1=Ebv_1, gamma_Ebv_1=gamma_Ebv_1,
-        Rb_2=Rb_2, gamma_Rb_2=gamma_Rb_2,
-        Ebv_2=Ebv_2, gamma_Ebv_2=gamma_Ebv_2,
-        shift_Rb=shift_Rb, lower_bound_Rb=lower_bound_Rb, upper_bound_Rb=upper_bound_Rb,
-        lower_bound_Ebv=lower_bound_Ebv, upper_bound_Ebv=upper_bound_Ebv
+        Rb_1=Rb_1, gamma_Rb_1=gamma_Rb_1, Ebv_1=Ebv_1, gamma_Ebv_1=gamma_Ebv_1,
+        lower_bound_Ebv_1=lower_bound_Ebv_1, lower_bound_Ebv_2=lower_bound_Ebv_1,
+        Rb_2=Rb_2, gamma_Rb_2=gamma_Rb_2, Ebv_2=Ebv_2, gamma_Ebv_2=gamma_Ebv_2,
+        lower_bound_Ebv_2=lower_bound_Ebv_2, upper_bound_Ebv_2=upper_bound_Ebv_2,
+        shift_Rb=shift_Rb, lower_bound_Rb=lower_bound_Rb, upper_bound_Rb=upper_bound_Rb
     )
     p_1 = probs[:, 0] / norm_1
     p_2 = probs[:, 1] / norm_2
