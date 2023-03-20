8202e3df7689ec73c25b795df600f9c297590fbb
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-03-20 17:54:49 +0100

    Added volumetric rate calc functions. Need to incorporate into model

--- src/model.py
+++ src/model.py
@@ -1,15 +1,20 @@
+import jax
+jax.config.update("jax_enable_x64", True)
 import numpy as np
 import numba as nb
+import jax.numpy as jnp
 import src.utils as utils
 import scipy.stats as stats
 import scipy.special as sp_special
 
 from NumbaQuadpack import quadpack_sig, dqags
 from astropy.cosmology import Planck18 as cosmo
+from diffrax import diffeqsolve, Tsit5, ODETerm, SaveAt, PIDController
 
 import src.preprocessing as prep
 
 NULL_VALUE = -9999.0
+H0_CONVERSION_FACTOR = 0.001022
 
 # ---------- E(B-V) PRIOR INTEGRAL ------------
 
@@ -277,6 +282,191 @@ def _Ebv_Rb_prior_convolution(
         status[i, 1] = s2
 
     return probs, status
+
+# ---------- z(T) ODE ------------
+
+def E(z, args):
+    _, Om, Ode, w0, wa = args
+    zp1 = 1+z
+    mass_term = Om * zp1**3.
+    de_term = Ode * zp1**(3.*(1.+w0+wa)) * jnp.exp(-3. * wa * z/zp1)
+    Ez = jnp.sqrt(mass_term + de_term)
+
+    return Ez
+
+def convolution_limits(cosmo, z, T0, T1):
+    times_z0_lower = (cosmo.age(z) - T0).value
+    times_z0_upper = (cosmo.age(z) - T1).value
+
+    return jnp.concatenate((times_z0_lower, times_z0_upper))
+
+def ode(
+    t, z, args
+):
+    H0, _, _, _, _ = args
+    zp1 = 1+z
+    Ez = E(z, args)
+
+    return -H0 * zp1 * Ez
+
+@jax.jit
+def redshift_at_times(
+    conv_limit_times,
+    t0, z0, cosmo_args
+):
+
+    idx_sort = jnp.argsort(conv_limit_times)
+    idx_unsort = jnp.argsort(idx_sort)
+    sorted_conv_limit_times = conv_limit_times[idx_sort]
+    t1 = sorted_conv_limit_times[-1]
+
+    term = ODETerm(ode)
+    solver = Tsit5()
+    saveat = SaveAt(ts=sorted_conv_limit_times)
+    stepsize_controller = PIDController(rtol=1e-8, atol=1e-8)
+
+    sol = diffeqsolve(
+        term, solver, t0=t0, t1=t1, y0=z0, dt0=None,
+        saveat=saveat, stepsize_controller=stepsize_controller,
+        args=cosmo_args
+    )
+
+    ts = jnp.column_stack(
+        jnp.split(
+            sol.ts[idx_unsort], 2
+        )
+    )
+    zs = jnp.column_stack(
+        jnp.split(
+            sol.ys[idx_unsort], 2
+        )
+    )
+    num_steps = sol.stats['num_steps']
+
+    return ts, zs, num_steps
+
+# ---------- N(z) integral ------------
+
+@nb.jit
+def lookback_time_integrand(
+    z, args
+):
+
+    H0, Om, w0, wa = args
+    H0_reduced = H0 * H0_CONVERSION_FACTOR
+    zp1 = 1+z
+    Ode = 1-Om
+    mass_term = Om * zp1**3.
+    de_term = Ode * zp1**(3.*(1.+w0+wa)) * np.exp(-3. * wa * z/zp1)
+    Ez = (mass_term + de_term)**0.5
+
+    value = 1./(H0_reduced * zp1 * Ez)
+
+    return value
+
+@nb.cfunc(quadpack_sig)
+def lookback_time_integral(z, args):
+    _args = nb.carray(args, (4,))
+    value = lookback_time_integrand(z, _args)
+
+    return value
+lookback_time_integral_ptr = lookback_time_integral.address
+
+@nb.jit
+def dtd_prompt(
+    t,
+    H0,
+    eta,
+):
+    h = H0/70.
+    return eta * h**2
+
+@nb.jit
+def dtd_delayed(
+    t,
+    H0,
+    eta,
+):
+    h = H0/70.
+    return eta * h**2 * t**-1 
+
+@nb.jit
+def SFH(z, H0):
+    
+    h = H0/70.
+    
+    numerator = 0.015 * (1+z)**2.7
+    denomenator = 1+((1+z) / 2.9)**5.6
+
+    return h * numerator / denomenator 
+
+@nb.jit
+def N_prompt_integrand(zp, args):
+    z, eta, H0 = args[:3]
+    lookback_time_args = args[2:]
+
+    jacobian = lookback_time_integrand(zp, lookback_time_args)
+    value = SFH(zp, H0) * dtd_prompt(zp, H0, eta) * jacobian
+
+    return value
+
+@nb.jit
+def N_delayed_integrand(zp, args):
+    z, eta, H0 = args[:3]
+    lookback_time_args = args[2:]
+
+    tau, _, _ = dqags(lookback_time_integral_ptr, z, zp, lookback_time_args)
+    jacobian = lookback_time_integrand(zp, lookback_time_args)
+    value = SFH(zp, H0) * dtd_delayed(tau, H0, eta) * jacobian
+
+    return value
+
+@nb.cfunc(quadpack_sig)
+def N_prompt_integral(zp, args):
+    _args = nb.carray(args, (6,))
+    value = N_prompt_integrand(zp, _args)
+
+    return value
+N_prompt_integral_ptr = N_prompt_integral.address
+
+@nb.cfunc(quadpack_sig)
+def N_delayed_integral(zp, args):
+    _args = nb.carray(args, (6,))
+    value = N_delayed_integrand(zp, _args)
+
+    return value
+N_delayed_integral_ptr = N_delayed_integral.address
+
+@nb.jit
+def volumetric_rates(
+    z, integral_limits,
+    H0, Om0, w0, wa,
+    eta_prompt, eta_delayed,
+    zinf=20.
+):
+    
+    
+    rates = np.zeros((len(z), 3), dtype=np.float64)
+    for i in range(len(z)):
+
+        zi = z[i]
+        z0 = integral_limits[i, 0]
+        z1 = integral_limits[i, 1]
+        prompt_args = np.array([zi, eta_prompt, H0, Om0, w0, wa], dtype=np.float64)
+        delayed_args = np.array([zi, eta_delayed, H0, Om0, w0, wa], dtype=np.float64)
+
+        N_prompt, _, _ = dqags(
+            N_prompt_integral_ptr, z0, z1, prompt_args
+        )
+        N_delayed, _, _ = dqags(
+            N_delayed_integral_ptr, z1, zinf, delayed_args
+        )
+
+        rates[i, 0] = N_prompt + N_delayed
+        rates[i, 1] = N_prompt
+        rates[i, 2] = N_delayed
+    
+    return rates
 
 # ---------- MODEL CLASS ------------
 
