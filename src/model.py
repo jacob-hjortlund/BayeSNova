071c2e794c2e63f2b0212f62821e429745ae6bca
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-04-03 17:37:16 +0200

    2

--- src/model.py
+++ src/model.py
@@ -950,6 +950,6 @@ class TrippModel():
         log_likelihood = self.log_likelihood(observables, covariances, **par_dict)
 
         # Test chang
-        # Test change 2
+        # Test change 3
 
         return log_likelihood
