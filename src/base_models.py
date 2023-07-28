36986233259c19ac61950db86718bc92f65229b2
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-07-28 21:16:17 +0200

    added base and constant weighting models + base mixture model

--- src/base_models.py
+++ src/base_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Model():
 
@@ -20,11 +21,49 @@ class Gaussian(Model):
     def covariance(self, **kwargs):
         
         raise NotImplementedError
+
+class Weighting(Model):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def calculate_weight(self, **kwargs):
+        
+        raise NotImplementedError
+
+class ConstantWeighting(Weighting):
+
+    def __init__(
+        self,
+        weight: float
+    ) -> None:
+        super().__init__()
+        self.weight = weight
     
+    def calculate_weight(
+        self,
+        redshift: float,
+        **kwargs
+    ):
+        
+        return np.ones_like(redshift) * self.weight
+
 class Mixture(Model):
 
     def __init__(
         self,
+        population_models: list[Gaussian],
+        weighting_model: Weighting
     ) -> None:
         super().__init__()
+
+        self.population_models = population_models
+        self.weighting_model = weighting_model
+    
+    def log_likelihood(
+        self,
+        redshift: float,
+        **kwargs
+    ) -> np.ndarray:
+        raise NotImplementedError
         
