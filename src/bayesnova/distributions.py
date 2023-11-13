bf701d3df7cd6d3685bc836f044ba5fa3540ff04
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-11-13 17:08:33 +0100

    added membership ratio

--- src/bayesnova/distributions.py
+++ src/bayesnova/distributions.py
@@ -97,7 +97,7 @@ class MultivariateNormal(Distribution):
         return dist.MultivariateNormal(mean, cov)
 
 
-class Gamma(Base):
+class Gamma(Distribution):
     concentration: Union[Array, zdx.Base]
     rate: Union[Array, zdx.Base]
 
@@ -120,7 +120,7 @@ class Gamma(Base):
         return dist.Gamma(concentration, rate)
 
 
-class TwoComponentMixture(Base):
+class TwoComponentMixture(Distribution):
     models: dict
     mixture_weight: Union[Array, zdx.Base]
 
@@ -147,13 +147,51 @@ class TwoComponentMixture(Base):
         else:
             raise AttributeError(f"{key} not in {self.models.keys()}")
 
-    def dist(self, *args, **kwargs):
-        mixture_weight = self.mixture_weight(*args, **kwargs)
-        mixture_weights = jnp.array([mixture_weight, 1.0 - mixture_weight])
+    def model_dists(self, *args, **kwargs):
+        return [model.dist(*args, **kwargs) for model in self.models.values()]
 
+    def dist(self, model_dists: list, mixture_weight: Array, *args, **kwargs):
+        mixture_weights = jnp.array([mixture_weight, 1.0 - mixture_weight])
         model = dist.MixtureGeneral(
             dist.Categorical(probs=mixture_weights),
-            [model.dist(*args, **kwargs) for model_name, model in self.models.items()],
+            model_dists,
         )
 
         return model
+
+    def log_membership_ratio(
+        self,
+        samples: Array,
+        model_dists: list,
+        mixture_weight: Array,
+        name: str = "log_membership_ratio",
+        *args,
+        **kwargs,
+    ):
+        log_prob_1 = model_dists[0].log_prob(samples) * jnp.log(mixture_weight)
+        log_prob_2 = model_dists[1].log_prob(samples) * jnp.log((1.0 - mixture_weight))
+
+        log_membership_ratio = log_prob_1 - log_prob_2
+        npy.deterministic(name, log_membership_ratio)
+
+    def sample(self, *args, **kwargs):
+        model_dists = self.model_dists(*args, **kwargs)
+        mixture_weight = self.mixture_weight(*args, **kwargs)
+
+        obs_key = self.name + "_obs"
+        value = npy.sample(
+            self.name,
+            self.dist(
+                model_dists=model_dists, mixture_weight=mixture_weight, *args, **kwargs
+            ),
+            obs=kwargs.get(obs_key),
+        )
+
+        self.log_membership_ratio(
+            value,
+            model_dists=model_dists,
+            mixture_weight=mixture_weight,
+            name=self.name + "_log_membership_ratio",
+        )
+
+        return value
